4a0966eae9b424011cd3a64020120887a4cc5b6b
SachaDBouvere <71385876+SachaDBouvere@users.noreply.github.com>
2021-04-08 13:28:43 +0200

    Update inver-marin-algue-code.py
    fin mais sans image et sans texte final

---
+++ inver-marin-algue-code.py
@@ -0,0 +1,190 @@
+label_marin_algovore: 
+#Image d’eau trouble (étang vert) avec des cailloux au fond si possible
+#Image de l’espèce partout sur terre  
+
+    n "Dans cette espèce qui s’est éparpillée sur tout le globe, plusieurs millions années après la dernière spéciation, plusieurs types d’individus existent mais 2 types se démarquent des autres,"
+    
+    n "les 12 et les 8 bras."
+    
+    n "Cette espèce possède une variété dans ses gènes et chaque individu peut donner naissance à une descendance avec 8 ou 12 membres."
+    
+    n "Un processus de spéciation sympatrique va croître chez nos Pluripodus-asichinus."
+    
+    n "En effet, au sein de cette même espèce leur mode de vie varie en fonction de leurs caractéristiques physiques."
+    
+    n "Notre population de Pluripodus-asichinus vit constamment chassé par les autres espèces du coin comme les Verdus visius."
+    
+    n "Effectivement, les prédateurs sont nombreux et certains sont même proches d’un point de vue évolutif car ce sont leurs lointains cousins, un des descendants des Aqua-jadus."
+
+    n "Par chance, nos petits bonhommes vivent en parfaite harmonie avec leur environnement riche en coraux et en rochers pour se cacher et rester bien agrippés à l’aide de leurs nombreux bras."
+
+    n "Grâce à ça, la majorité du temps, ils vont pouvoir réussir à fuir leurs prédateurs et donc rester en vie."
+
+    n "Cependant, dans la population, la différence de membres influence beaucoup le mode de vie des individus."
+
+    n "La population des plus membrés est plus souvent à découvert et donc plus souvent victime des attaques des prédateurs."
+
+    n "Malgré cela, ils n’ont aucun problème à se reproduire grâce à leur bonne visibilité et grâce au fait que les femelles aiment les mâles bien membrés."
+
+    n "Le mode de vie des moins membrés est plus calme et moins risqué car ils sont plus cachés que leurs congénères,"
+    
+    n "ils vont donc moins se reproduire mais auront une longévité bien plus grande."
+
+# monsieur ADN apparait 
+
+    m "Les 8 et les 12 bras peuvent se reproduire entre eux."
+    
+    m "Cependant, les individus vont privilégier la reproduction avec leurs semblables. Les 8 bras avec les 8 bras et les 12 avec les 12."
+
+    menu:
+        n "Quels types d’individus voulez-vous suivre ?"
+        "12 bras":
+        jump Douze_bras
+        "8 bras":
+
+            n "Le fait d’avoir 8 bras est un avantage évolutif."
+
+    n "Ils arrivent mieux à survivre car ils sont moins encombrés et peuvent donc se cacher plus rapidement et plus efficacement."
+
+    n "Cela leur donne un avantage sur ceux qui ont plus de membres car ils ont « plus de chance » de se faire dévorer par les prédateurs à cause de leur morphologie plus imposante."
+
+    n "De plus, à cause du phénomène de spéciation sympatrique,"
+        
+    n "notre population à 8 membres sera moins en contact avec la seconde population au moment de se reproduire dans le fond de l’eau."
+
+#  Image de pluripodus sur un rocher en train de manger un corail.
+
+    n "Notre population de pluripodus-asichinus à 8 membres ont une certaine stabilité dans leur mode de vie dans le fond de l’eau."
+
+    n "Ils vivent et se reproduisent sans se soucier de rien mais dans la vie rien n’est constant indéfiniment."
+
+# image d’une tempête marine 
+
+    n "Un Événement météorologique va provoquer dans la zone de forts courants marin,"
+
+    n ", ce qui va conduire à faire migrer de façon involontaire nos une grosse partie de nos Pluripodus-asichinus de la zone pélagique là où les courants sont les plus puissants."
+
+    n "La population à 8 membres possédant un nombre de patte réduit comparé à leurs congénères ne vont pas être affecté par ce courant car ils se cachent dans les rochers au fond de l’eau grâce à leur morphologie."
+
+    n "Les 12 membres se font donc emporter de la zone par les courant laissant tout le territoire à la population à 8 membres."
+
+    n "Au cours des millions d’années qui ont suivi cet événement et des générations, nos Pluripodus-asichinus ne pouvant plus remonter se nourrir de leurs algues favorites à causes des courants fort."
+
+    n "Ils ont dû se rabattre sur les coraux mous lumineux au fond de l’eau."
+
+    n "Après de nombreuses générations une partie de la population va acquérir la capacité de changer et contrôler les pigments de leur corps et ce,"
+    
+    n "grâce à leur nouvelle alimentation tandis que d’autre n’acquerront jamais cette capacité."
+
+# Image de pluripodus arc-en-ciel sur un rocher avec ses membres en l’air 
+
+    n "Cette mutation va être à l’origine d’une spéciation de l’espèce qu’on l’on nommera dorénavant scintillus-asichinus."
+
+    n "Ceux-ci vont élaborer après des millions d’années de perfection,"
+
+    n "une danse nuptiale dans le but d’attirer des partenaires et transmettre leurs gènes à leurs descendants,"
+
+    n "grâce à l’exposition et à la gesticulation de leurs membres colorés."
+
+    n "Les plus attractifs vont plus facilement se reproduire que les autres tandis que les courageux et solitaire incolore,"
+    
+    n "continue de lutter pour leur survie malgré une concurrence déloyale des colorés."
+
+# Monsieur ADN apparait 
+
+    m "La sélection sexuelle en action est un mécanisme de sélection naturelle basé sur la compétition,"
+    
+    m "dans le but de s’accoupler grâce à des traits héréditaires qui avantagent leur chance de reproductions auprès de leur partenaire éventuel."
+
+    menu:
+        n "Qui voulez-vous suivre entre les colorés et les incolores ?"
+        "colorés":
+        jump algue_colorés 
+        "incolores"
+
+         n "Malheureusement vous ne continuerez pas l’aventure avec les scintillus-asichinus incolore,"
+        
+    n "c’est une espèce qui ne parviendra pas survivre au cours des générations à cause d’une concurrence trop importante et une mauvaise adaptation."
+
+    n "Ils se trouvent dans un cul de sac évolutif,"
+        
+    n "sans le vouloir la mère évolution n’est pas toujours tendre avec ses enfants et certaines populations sont incapable de pouvoir concurrencer les mieux adapté et sont donc condamnés à disparaitre."
+
+# retour texte final
+ return 
+
+ label algue_colorés
+
+    n "Vous décidez de continuer votre aventure avec les scintillus-asichinus colorés."
+
+    n "Le résultat de la sélection sexuelle va faire qu’au cours des générations la population va devenir de plus en plus complexe au niveau des couleurs et de la technique de séduction,"
+    
+    n "afin de toujours montrer qu’ils sont le meilleur parti possible pour leur partenaire et ainsi transmettre leurs caractères aux générations suivantes."
+
+return 
+
+label Douze_bras
+
+    n "Notre population de pluripodus-asichinus à 12 membres ont une certaine stabilité dans leur mode de vie dans leur zone pélagique."
+
+    n "Ils vivent et se reproduisent sans se soucier de rien mais dans la vie rien n’est constant."
+
+    n "Un Événement météorologique va provoquer dans la zone de forts courants marins,"
+    
+    n "ce qui va conduire à faire migrer de façon involontaire nos Pluripodus-asichinus de la zone pélagique là où les courants sont les plus puissants."
+
+    n "Alors que ceux qui sont dans le fond de l’eau continuent leur vie sans se soucier de ce qui se passe au-dessus."
+
+    n "La population à 12 membres emportée par le courant marin dans la nouvelle zone, a une nouvelle source d’alimentation."
+
+    n "Des algues sous-marines très riche en calcium contrairement à leur précédente source d’alimentation dans leur zone d’origine."
+
+    n "Cette différence de milieux et ce nouveau régime, après plusieurs millions d’années,"
+
+    n "va provoquer dans notre population une spéciation de l’espèce et la naissance des dodécae-coquillus."
+
+    n "L’excès de calcium dans leur organisme va engendrer au cours de nombreuses générations une coquille."
+
+    n "Dans notre population, plusieurs types de coquilles vont se développer et certains ne sont pas adaptés et sont non viables alors que d’autres sont parfaitement adaptés à l’environnement."
+
+    n "Les individus avec les caractères les plus intéressants vont se reproduire entre eux par sélection sexuelle grâce à une danse nuptiale."
+
+# Monsieur ADN apparait 
+
+    m "La sélection sexuelle en action est un mécanisme de sélection naturelle basé sur la compétition dans le but de s’accoupler"
+    
+    m "grâce à des traits héréditaires qui avantagent leur chance de reproductions auprès de leur partenaire éventuel."
+
+    m "Deux types de coquilles vont se démarquer au cours des millions d’années écoulées,"
+
+    m "la grosse coquille qui va pouvoir permettre une symbiose avec un certain type de cnidaire et la petite coquille qui ne permet pas de symbiose mais qui va leur offrir une bien meilleure mobilité."
+
+    menu:
+        n "Voulez-vous vous aventurer avec les grosses coquilles ou les petites coquilles ?"
+        "La grosse coquille":
+            jump grosse_coquille
+        "La petite coquille":
+
+            n "Vous avez décidé de continuer votre aventure avec les dodécaius-coquillus avec une petite coquille."
+
+    n "Cette coquille permet à l’espèce de conserver une excellente mobilité et une rapidité raisonnable pour s’enfuir lors d’une attaque."
+
+    n "De plus cette petite coquille n’étant pas trop encombrante elle représente un avantage dans la sélection sexuelle appliqué lors d’une danse nuptiale pour attirer leur partenaire,
+    
+    n "contrairement aux grosses coquilles qui n’ont plus assez d’adresse."
+
+#texte final 
+
+return 
+
+label grosse_coquille
+
+    n "Vous avez décidé de continuer votre aventure avec les dodécae-coquillus avec la grosse coquille,"
+    
+    n "ce qui leur permet de faire une symbiose avec des cnidaires leur offrant un moyen de protection optimal grâce à leurs cellules urticantes contre les prédateurs."
+
+    n "Mais ceux-ci doivent passer leur temps à se nourrir pour subvenir à leurs besoins mais également aux besoins de leur symbiote pour éviter qu’il ne devienne un parasite."
+
+# texte final
+
+return 
